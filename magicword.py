bf7d418eddff48f6ed77a100c53ef4af82f89014
pravintargaryen <96950453+pravintargaryen@users.noreply.github.com>
2022-11-02 12:24:27 +0530

    Add files via upload

---
+++ magicword.py
@@ -0,0 +1,18 @@
+print("Jurassic Park System Security Interface")
+print ("Version 4.0.5, Alpha E")
+print("Ready...")
+print ("> access security ")
+print("access : PERMISSION DENIED ")
+print ("> access security grid")
+print("access : PERMISSION DENIED ")
+print ("> access main security grid")
+print("access : PERMISSION DENIED...and... ")
+print("YOU DIDN'T SAY THE MAGIC WORD\n" * 20)
+
+from IPython.display import Image, display
+
+url = "https://www.waywardsparkles.com/wp-content/uploads/2021/05/the-magic-word.gif"
+
+while True :
+  display(Image(data=url, width=200, height=200))
+  break
